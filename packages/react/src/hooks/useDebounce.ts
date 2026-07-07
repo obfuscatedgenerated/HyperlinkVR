f257a73b1dd9f7040a4014331d80d535b467b2e2
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-07-07 18:47:16 +0100

    debounce settings with leading edge

--- packages/react/src/hooks/useDebounce.ts
+++ packages/react/src/hooks/useDebounce.ts
@@ -1,17 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
-export const useDebounce = <T = any>(value: T, delay: number) => {
+export const useDebounce = <T = any>(value: T, delay: number, leading_edge = false) => {
     const [debounced, setDebounced] = useState(value);
 
+    const cooldown = useRef(false);
+
     useEffect(() => {
-        const handler = setTimeout(() => {
+        if (leading_edge && !cooldown.current) {
+            setDebounced(value);
+        }
+
+        cooldown.current = true;
+
+        const timeout = setTimeout(() => {
+            cooldown.current = false;
             setDebounced(value);
         }, delay);
 
         return () => {
-            clearTimeout(handler);
+            clearTimeout(timeout);
         };
-    }, [value, delay]);
+    }, [value, delay, leading_edge]);
 
     return debounced;
 };
--- packages/react/src/hooks/useSetting.ts
+++ packages/react/src/hooks/useSetting.ts
@@ -8,10 +8,12 @@ import { SettingKey, settings_def } from "@hyperlinkvr/types";
 import { useCallback, useEffect, useState } from "react";
 
 import { useStorageEngines } from "../contexts";
+import { useDebounce } from "./useDebounce";
 
 export const useSettingWithEngines = <K extends SettingKey>(
     key: K,
-    storage: { sync?: StorageEngine<"sync">; local?: StorageEngine<"local"> }
+    storage: { sync?: StorageEngine<"sync">; local?: StorageEngine<"local"> },
+    debounce_delay = 500
 ) => {
     const [value, setValue] = useState<(typeof settings_def)[K]["default_value"]>(
         settings_def[key].default_value
@@ -24,18 +26,28 @@ export const useSettingWithEngines = <K extends SettingKey>(
 
     const update_value = useCallback(
         (new_value: (typeof settings_def)[K]["default_value"]) => {
-            update_setting(key, new_value, storage);
+            //update_setting(key, new_value, storage);
             setValue(new_value);
         },
         [key, storage]
     );
 
+    // debounce setting updates (set delay to 0 to disable, but not recommended)
+    // with leading edge for quick responses to one off events
+    const debounced_value = useDebounce(value, debounce_delay, true);
+
+    useEffect(() => {
+        update_setting(key, debounced_value, storage);
+    }, [key, debounced_value, storage]);
+
     return [value, update_value] as const;
 };
 
-export const useSetting = <K extends SettingKey>(key: K) => {
+export const useSetting = <K extends SettingKey>(key: K, debounce_delay = 500) => {
     const storage_engines = useStorageEngines();
-    return useSettingWithEngines(key, storage_engines);
+    return useSettingWithEngines(key, storage_engines, debounce_delay);
 };
 
-// TODO: automatically debounce setting setter, with boolean to disable debounce if needed (browser storage is ratelimited)
+// TODO: use context to make it sync to business logic immediately even if not commited. leading edge helps but not perfect. make it optional to use setting context too as its a little annoying
+// (also currently the debounces are per useSetting call, context would actually ensure global debounce)
+// would be nice if the existing hook could contextually figure out if its in a context or not, and rather than error just fallback to unique instance (needs little app code change!)
