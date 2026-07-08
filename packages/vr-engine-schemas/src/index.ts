78dde3bdcbda0f7cc3a812c93d542185d2b21c55
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-07-08 02:10:04 +0100

    todo

--- packages/vr-engine-schemas/src/index.ts
+++ packages/vr-engine-schemas/src/index.ts
@@ -3,3 +3,4 @@ export * from "./events";
 
 // TODO: setting player monitors, player physics etc
 // TODO: setting env props (skybox props, world colliders etc)
+// TODO: setting spectator display mode (force camera mode, angle, or hide completely, maybe even ability to set custom tab stream or iframe)
